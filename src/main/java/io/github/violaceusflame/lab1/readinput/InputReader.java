929bc30f177758bacd72c68534d79d1d0d6e4b58
Alexander <lushmetry@gmail.com>
2024-10-12 13:57:00 +0300

    [lab 1] removed synchronized methods

--- src/main/java/io/github/violaceusflame/lab1/readinput/InputReader.java
+++ src/main/java/io/github/violaceusflame/lab1/readinput/InputReader.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class InputReader {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static synchronized String readInput() {
+    public static String readInput() {
         return scanner.nextLine();
     }
 }
